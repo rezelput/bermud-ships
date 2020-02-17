87133688089214d5987fb0c6276c790818ae242c
Sofia <gn17o@yandex.ru>
2020-02-17 21:18:17 +0300

    run-ships-C-v1.0
    run ships v1.0 , no forms QT

---
+++ run-ships-v1.c
@@ -0,0 +1,22 @@
+include <stdio.h>
+void go2W(int * lat, int * lon)
+{
+    *lat = *lat -1;
+    *lon = *lon +1;
+}
+void go2E(int * lt1, int * ln2)
+{
+    while (lt1 != 'x1')
+    {
+        char side[4];
+puts ("Change side of the world: ")
+fgets(side, change(side), stdin)
+    }
+}
+int main(void)
+{
+   int latitude = 32;
+   int longtitude = 64;
+   go2W(&latitude, &longtitude)
+   printf("Coord: [%i,%i]\n", latitude, longtitude) 
+}
